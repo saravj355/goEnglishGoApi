fb60c78c8de7b509e1206e4e6d70bc6c3b9084d2
Sara Valle <svj971@gmail.com>
2022-11-17 21:46:14 -0500

    initial commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,6 @@ import express from "express";
 const Router = express.Router();
 import verbs from "./verbsRoutes.js";
 
-Router.get("/", (req, res) => {
-	res.send("Api");
-});
-
 Router.use("/verbs", verbs);
 
 export default Router;
--- src/server.js
+++ src/server.js
@@ -6,10 +6,6 @@ import router from "./routes/index.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-	res.send("Success");
-});
-
 app.use("/api", router);
 
 const init = () => {
